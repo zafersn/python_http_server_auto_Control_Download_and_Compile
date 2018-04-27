42dad5507e020fa7b9ca446fc1480132c3ae97ed
Zafer M.ŞEN <zafersn93@gmail.com>
2018-04-27 10:32:40 +0300

    update

--- listServer.py
+++ listServer.py
@@ -39,14 +39,22 @@ class ConnctAndDownld():
                                 if not os.path.exists("test1.sh"):
                                     self.createFile("ThreadControl",file,1)
                                 else:
-                                    print "test1.nooooo %s " % file[file.index("/file/")+6:][-1:]
-                        elif file[file.index("/file/")+6:][-1:] == "/":
-                            self.url=self.url +"/" +file[file.index("/file/")+6:-1]
-                            print "url: %s " % self.url
-                            self.fileList()                            
-                        elif not (file.index("ThreadControl/") == -1):
-                            print "ThreadControl/ file..."
-                            self.createFile("ThreadControl",file,0)
+                                    print "bole bir dosya var indirilemez... %s " % file[file.index("/file/")+6:][-1:]
+                        elif file[file.index("/file/")+6:][-1:] == "/":                                
+                                self.url=self.url +"/" +file[file.index("/file/")+6:-1]
+                                print "url: %s " % self.url
+                                self.fileList()                            
+                        elif "ThreadControl/" in file:#elif not (file.index("ThreadControl/") == -1):
+                                print "ThreadControl/ file..."
+                                self.createFile("ThreadControl",file,0)
+                        elif file[file.index("/file/")+6:] == "KW41Z-hostcontrol-serial-upgrade-vX.Y.Z.bin":
+                                if os.path.exists("KW41Z-hostcontrol-serial-upgrade-vX.Y.Z.bin"):
+                                        self.callShellCommand("rm KW41Z-hostcontrol-serial-upgrade-vX.Y.Z.bin")
+                                        print "siliniyor..."
+                                        
+                                else:
+                                        print "KW41Z-hostcontrol-serial-upgrade-vX.Y.Z.bin yok .. indiriliyor.."
+                                self.createFile("ThreadControl",file,1)
                         else:
                             print "bilinmiyor..."
                         
@@ -57,6 +65,7 @@ class ConnctAndDownld():
 		else :
                     print "dosya var"
                 r = requests.get(url)
+                name=""
                 if isCreate ==1:
                     name=url[url.index("/file/")+6:]# url'de /file/ ifadesinden sonraki dosya ismini aliyoruz.
                 elif isCreate == 0:
@@ -75,17 +84,20 @@ class ConnctAndDownld():
                         #self.sFcount-=1
                         #print "%s tasindi..." % name
                         
-                if self.sFcount==12: # toplamda 12 tane dosya inmis ise derlemeyi baslatiyoruz.
-                        self.compile()
+                if self.sFcount==13: # toplamda 12 tane dosya inmis ise derlemeyi baslatiyoruz.
+                       # self.compile()
+                        print "compileeeeeeeee......"
                 print self.sFcount
 				
 				
 	def compile (self):
 		#subprocess.call('chmod +777 ~/test/selam/test1.sh')
                 #subprocess.call('chown -R $USER selam/test1.sh')
+                self.callShellCommand("chmod +777 test1.sh")
 		subprocess.call(shlex.split('./test1.sh')) #shell command calistiriyoruz...
 		
-
+        def callShellCommand(self,command):
+                subprocess.call(shlex.split(command))
 
 if __name__=='__main__':
 
